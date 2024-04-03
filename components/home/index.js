97af84d967f4f26809198d0b506d8d92b0e789d3
MontoyaAndres <andresmontoyafcb@gmail.com>
2024-04-03 17:25:45 -0500

    Feat: mint pet

--- components/home/index.js
+++ components/home/index.js
@@ -249,51 +249,53 @@ export const Home = (props) => {
             </div>
           )}
         </div>
-        <div className="subelements">
-          <Typography
-            gutterBottom
-            variant="h5"
-            component="div"
-            style={{ fontWeight: 600 }}
-          >
-            Top 10 DeFi-Pets
-          </Typography>
-          <div className="leaderboard">
-            <List sx={{ bgcolor: "background.paper" }}>
-              {new Array(10).fill(null).map((_, index) => (
-                <>
-                  <ListItem alignItems="flex-start">
-                    <ListItemAvatar>
-                      <Avatar
-                        alt="Remy Sharp"
-                        src="https://cdn-icons-png.flaticon.com/512/5957/5957125.png"
+        {mintAlready && (
+          <div className="subelements">
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              style={{ fontWeight: 600 }}
+            >
+              Top 10 DeFi-Pets
+            </Typography>
+            <div className="leaderboard">
+              <List sx={{ bgcolor: "background.paper" }}>
+                {new Array(10).fill(null).map((_, index) => (
+                  <>
+                    <ListItem alignItems="flex-start">
+                      <ListItemAvatar>
+                        <Avatar
+                          alt="Remy Sharp"
+                          src="https://cdn-icons-png.flaticon.com/512/5957/5957125.png"
+                        />
+                      </ListItemAvatar>
+                      <ListItemText
+                        primary="Brunch this weekend?"
+                        secondary={
+                          <Fragment>
+                            <Typography
+                              sx={{ display: "inline" }}
+                              component="span"
+                              variant="body2"
+                              color="text.primary"
+                            >
+                              Ali Connors
+                            </Typography>
+                            {
+                              " — I'll be in your neighborhood doing errands this…"
+                            }
+                          </Fragment>
+                        }
                       />
-                    </ListItemAvatar>
-                    <ListItemText
-                      primary="Brunch this weekend?"
-                      secondary={
-                        <Fragment>
-                          <Typography
-                            sx={{ display: "inline" }}
-                            component="span"
-                            variant="body2"
-                            color="text.primary"
-                          >
-                            Ali Connors
-                          </Typography>
-                          {
-                            " — I'll be in your neighborhood doing errands this…"
-                          }
-                        </Fragment>
-                      }
-                    />
-                  </ListItem>
-                  <Divider variant="inset" component="li" />
-                </>
-              ))}
-            </List>
+                    </ListItem>
+                    <Divider variant="inset" component="li" />
+                  </>
+                ))}
+              </List>
+            </div>
           </div>
-        </div>
+        )}
         <div className="titles">
           <Typography variant="h4" className="title">
             My DeFi Pets
